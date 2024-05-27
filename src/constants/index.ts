201941e25da07dc6cf0c4c5425cf57269f079dee
Maestro1982 <kris.tote@telenet.be>
2024-05-27 16:48:27 +0200

    implement hero section for the landing page, drag n drop multiple pictures in public folder

---
+++ src/constants/index.ts
@@ -0,0 +1,24 @@
+export const userImages = [
+  {
+    id: 1,
+    src: '/users/user-1.png',
+  },
+  {
+    id: 2,
+    src: '/users/user-2.png',
+  },
+  {
+    id: 3,
+    src: '/users/user-3.png',
+  },
+  {
+    id: 4,
+    src: '/users/user-4.jpg',
+  },
+  {
+    id: 5,
+    src: '/users/user-5.jpg',
+  },
+];
+
+export const STARS = 5;
