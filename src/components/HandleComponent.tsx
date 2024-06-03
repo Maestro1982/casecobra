8101dc7fecaba5a082a3a40a794159c79533ef37
Maestro1982 <kris.tote@telenet.be>
2024-06-03 15:55:00 +0200

    implement configurator page part 1, setup database

---
+++ src/components/HandleComponent.tsx
@@ -0,0 +1,6 @@
+const HandleComponent = () => {
+  return (
+    <div className='h-5 w-5 rounded-full shadow border bg-white border-zinc-200 transition-none hover:bg-primary' />
+  );
+};
+export default HandleComponent;
