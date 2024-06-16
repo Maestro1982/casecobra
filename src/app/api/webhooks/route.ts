291352cdce0d5eca84d8133e8a5dd484c08d1780
Maestro1982 <kris.tote@telenet.be>
2024-06-16 11:22:59 +0200

    update route.ts webhook api

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -5,34 +5,27 @@ import { NextResponse } from 'next/server';
 import { stripe } from '@/lib/stripe';
 import { db } from '@/db';
 
-interface ExtendedSession extends Stripe.Checkout.Session {
-  shipping?: {
-    address: Stripe.Address;
-  };
-}
-
 export async function POST(req: Request) {
   try {
     const body = await req.text();
-    const signature = headers().get('stripe-signature');
+    const signature = headers().get('stripe-signature') as string;
 
     if (!signature) {
       return new Response('Invalid signature', { status: 400 });
     }
 
-    const event = stripe.webhooks.constructEvent(
+    let event = stripe.webhooks.constructEvent(
       body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
 
     if (event.type === 'checkout.session.completed') {
-      const session = event.data.object as ExtendedSession;
-      console.log('Checkout session completed:', session);
-
-      if (!session.customer_details?.email) {
-        throw new Error('Missing user email');
+      if (!event.data.object.customer_details?.email) {
+        throw new Error('User email missing');
       }
+
+      const session = event.data.object as Stripe.Checkout.Session;
 
       const { userId, orderId } = session.metadata || {
         userId: null,
@@ -44,10 +37,7 @@ export async function POST(req: Request) {
       }
 
       const billingAddress = session.customer_details!.address;
-      const shippingAddress = session.shipping!.address;
-
-      console.log('Billing address:', billingAddress);
-      console.log('Shipping address:', shippingAddress);
+      const shippingAddress = session.shipping_details!.address;
 
       const updatedOrder = await db.order.update({
         where: {
@@ -65,7 +55,6 @@ export async function POST(req: Request) {
               state: shippingAddress!.state,
             },
           },
-
           billingAddress: {
             create: {
               name: session.customer_details!.name!,
@@ -78,13 +67,11 @@ export async function POST(req: Request) {
           },
         },
       });
-
-      console.log('Updated order:', updatedOrder);
     }
 
     return NextResponse.json({ result: event, ok: true });
   } catch (err) {
-    console.error('Error processing webhook:', err);
+    console.error(err);
 
     return NextResponse.json(
       { message: 'Something went wrong', ok: false },
