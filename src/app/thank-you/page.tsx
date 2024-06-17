a07ab94cd871695f2b05079cb8328302a878db9c
Maestro1982 <kris.tote@telenet.be>
2024-06-17 15:57:45 +0200

    Implementing thank you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import { Suspense } from 'react';
+
+import ThankYou from '@/app/thank-you/ThankYou';
+
+const ThankYouPage = () => {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  );
+};
+export default ThankYouPage;
